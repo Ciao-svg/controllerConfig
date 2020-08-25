a96689012204dc97a6fb79b9777836bd4695d1dd
Ciao-svg <56894431+Ciao-svg@users.noreply.github.com>
2020-08-25 20:49:16 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,43 @@
+function press_A() {
+  key.control.simulate = {
+    "key": "%%akey%%"
+  }
+  return 0;
+}
 
+function press_B() {
+  key.control.simulate = {
+    "key": "%%bkey%%"
+  }
+  return 0;
+}
+
+function press_X() {
+  key.control.simulate = {
+    "key": "%%xkey%%"
+  }
+  return 0;
+}
+
+function press_Y() {
+  key.control.simulate = {
+    "key": "%%ykey%%"
+  }
+  return 0;
+}
+
+if(keyDown.controller("A") and controllerConnected = true) {
+  press_A();
+}
+
+if(keyDown.controller("B") and controllerConnected = true) {
+  press_B();
+}
+
+if(keyDown.controller("X") and controllerConnected = true) {
+  press_X();
+}
+
+if(keyDown("Y") and controllerConnected = true) {
+  press_Y();
+}
